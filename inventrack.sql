60f1a8e69090d12aa709b48804027c63160324b9
Jessica <jessica.amanda1410@gmail.com>
2026-03-03 10:18:07 +0700

    mengubah readme

--- inventrack.sql
+++ inventrack.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 23, 2024 at 02:57 AM
+-- Generation Time: Mar 03, 2026 at 03:09 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `inventaris`
+-- Database: `inventrack`
 --
 
 -- --------------------------------------------------------
@@ -28,20 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `anggota` (
-  `id_anggota` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `nama_anggota` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `jk` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `alamat` text COLLATE utf8mb4_general_ci NOT NULL,
-  `no_hp` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  `pass_foto` varchar(200) COLLATE utf8mb4_general_ci NOT NULL
+  `id_anggota` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_anggota` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `jk` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `alamat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `no_hp` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pass_foto` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `anggota`
---
-
-INSERT INTO `anggota` (`id_anggota`, `nama_anggota`, `jk`, `alamat`, `no_hp`, `pass_foto`) VALUES
-('AG001', 'agus', 'Perempuan', 'lumajang', '2910034', '');
 
 -- --------------------------------------------------------
 
@@ -50,20 +43,13 @@ INSERT INTO `anggota` (`id_anggota`, `nama_anggota`, `jk`, `alamat`, `no_hp`, `p
 --
 
 CREATE TABLE `barang` (
-  `id_barang` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_barang` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `id_jenisbarang` int NOT NULL,
-  `nama_barang` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `spesifikasi` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_barang` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `spesifikasi` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `jumlah` int NOT NULL,
-  `foto_barang` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
+  `foto_barang` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `barang`
---
-
-INSERT INTO `barang` (`id_barang`, `id_jenisbarang`, `nama_barang`, `spesifikasi`, `jumlah`, `foto_barang`) VALUES
-('BR004', 3, 'Laptop ASUS ', 'core i3', 3, '');
 
 -- --------------------------------------------------------
 
@@ -73,15 +59,8 @@ INSERT INTO `barang` (`id_barang`, `id_jenisbarang`, `nama_barang`, `spesifikasi
 
 CREATE TABLE `jenis_barang` (
   `id_jenisbarang` int NOT NULL,
-  `jenisbarang` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
+  `jenisbarang` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `jenis_barang`
---
-
-INSERT INTO `jenis_barang` (`id_jenisbarang`, `jenisbarang`) VALUES
-(3, 'Laptop');
 
 -- --------------------------------------------------------
 
@@ -91,33 +70,13 @@ INSERT INTO `jenis_barang` (`id_jenisbarang`, `jenisbarang`) VALUES
 
 CREATE TABLE `kembalikan` (
   `id_kembali` int NOT NULL,
-  `id_anggota` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `id_barang` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_anggota` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `id_barang` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `jumlah_kembali` int NOT NULL,
   `tgl_pinjam` date NOT NULL,
   `tgl_kembali` date NOT NULL,
   `tgl_kembalikan` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `kembalikan`
---
-
-INSERT INTO `kembalikan` (`id_kembali`, `id_anggota`, `id_barang`, `jumlah_kembali`, `tgl_pinjam`, `tgl_kembali`, `tgl_kembalikan`) VALUES
-(15, 'AG001', 'BR001', 1, '2024-09-12', '2024-09-12', '2024-09-12'),
-(16, 'AG001', 'BR004', 4, '2024-10-08', '2024-10-22', '2024-10-08'),
-(17, 'AG001', 'BR004', 2, '2024-10-08', '2024-10-24', '2024-10-08'),
-(18, 'AG001', 'BR004', 1, '2024-10-08', '2024-10-16', '2024-10-08'),
-(19, 'AG001', 'BR004', 1, '2024-10-08', '2024-10-25', '2024-10-08'),
-(20, 'AG001', 'BR004', 1, '2024-10-08', '2024-10-08', '2024-10-08'),
-(21, 'AG001', 'BR004', 1, '2024-10-08', '2024-10-08', '2024-10-08'),
-(22, 'AG001', 'BR004', 2, '2024-10-08', '2024-10-08', '2024-10-08'),
-(23, 'AG001', 'BR004', 2, '2024-10-08', '2024-10-08', '2024-10-08'),
-(24, 'AG001', 'BR004', 1, '2024-10-08', '2024-10-08', '2024-10-08'),
-(25, 'AG001', 'BR004', 2, '2024-10-08', '2024-10-08', '2024-10-08'),
-(26, 'AG001', 'BR004', 2, '2024-10-08', '2024-10-08', '2024-10-08'),
-(27, 'AG001', 'BR004', 1, '2024-10-08', '2024-10-08', '2024-10-08'),
-(28, 'AG001', 'BR004', 1, '2024-10-08', '2024-10-08', '2024-10-08');
 
 --
 -- Triggers `kembalikan`
@@ -139,10 +98,10 @@ DELIMITER ;
 
 CREATE TABLE `login` (
   `id` int NOT NULL,
-  `nama` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `level` varchar(20) COLLATE utf8mb4_general_ci NOT NULL
+  `nama` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `level` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -160,9 +119,9 @@ INSERT INTO `login` (`id`, `nama`, `username`, `password`, `level`) VALUES
 --
 
 CREATE TABLE `pinjam` (
-  `id_pinjam` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `id_anggota` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `id_barang` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_pinjam` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `id_anggota` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `id_barang` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `jumlah_pinjam` int NOT NULL,
   `tgl_pinjam` date NOT NULL,
   `tgl_kembali` date NOT NULL
@@ -223,13 +182,13 @@ ALTER TABLE `pinjam`
 -- AUTO_INCREMENT for table `jenis_barang`
 --
 ALTER TABLE `jenis_barang`
-  MODIFY `id_jenisbarang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_jenisbarang` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `kembalikan`
 --
 ALTER TABLE `kembalikan`
-  MODIFY `id_kembali` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id_kembali` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `login`
--- readme.rst
+++ readme.rst
@@ -1,71 +1,24 @@
-###################
-What is CodeIgniter
-###################
+Aplikasi Inventaris & Peminjaman Barang
+Sistem Informasi Inventaris dan Peminjaman Barang berbasis **CodeIgniter 3** untuk membantu pengelolaan data barang, anggota, transaksi peminjaman, serta laporan secara terstruktur dan terintegrasi.
 
-CodeIgniter is an Application Development Framework - a toolkit - for people
-who build web sites using PHP. Its goal is to enable you to develop projects
-much faster than you could if you were writing code from scratch, by providing
-a rich set of libraries for commonly needed tasks, as well as a simple
-interface and logical structure to access these libraries. CodeIgniter lets
-you creatively focus on your project by minimizing the amount of code needed
-for a given task.
+Teknologi yang Digunakan:
+* PHP (Rekomendasi 7.4, karena CI3 kurang stabil di PHP 8.1+)
+* MySQL / MariaDB
+* CodeIgniter 3
+* Bootstrap (untuk tampilan dashboard)
+* XAMPP / Laragon
 
-*******************
-Release Information
-*******************
+Fitur Utama
+1. Login Sistem
+2. Dashboard
+3. Manajemen Anggota
+4. Manajemen Barang
+5. Transaksi Peminjaman
+6. Laporan
 
-This repo contains in-development code for future releases. To download the
-latest stable release please visit the `CodeIgniter Downloads
-<https://codeigniter.com/download>`_ page.
-
-**************************
-Changelog and New Features
-**************************
-
-You can find a list of all changes for each release in the `user
-guide change log <https://github.com/bcit-ci/CodeIgniter/blob/develop/user_guide_src/source/changelog.rst>`_.
-
-*******************
-Server Requirements
-*******************
-
-PHP version 5.6 or newer is recommended.
-
-It should work on 5.3.7 as well, but we strongly advise you NOT to run
-such old versions of PHP, because of potential security and performance
-issues, as well as missing features.
-
-************
-Installation
-************
-
-Please see the `installation section <https://codeigniter.com/userguide3/installation/index.html>`_
-of the CodeIgniter User Guide.
-
-*******
-License
-*******
-
-Please see the `license
-agreement <https://github.com/bcit-ci/CodeIgniter/blob/develop/user_guide_src/source/license.rst>`_.
-
-*********
-Resources
-*********
-
--  `User Guide <https://codeigniter.com/docs>`_
--  `Contributing Guide <https://github.com/bcit-ci/CodeIgniter/blob/develop/contributing.md>`_
--  `Language File Translations <https://github.com/bcit-ci/codeigniter3-translations>`_
--  `Community Forums <http://forum.codeigniter.com/>`_
--  `Community Wiki <https://github.com/bcit-ci/CodeIgniter/wiki>`_
--  `Community Slack Channel <https://codeigniterchat.slack.com>`_
-
-Report security issues to our `Security Panel <mailto:security@codeigniter.com>`_
-or via our `page on HackerOne <https://hackerone.com/codeigniter>`_, thank you.
-
-***************
-Acknowledgement
-***************
-
-The CodeIgniter team would like to thank EllisLab, all the
-contributors to the CodeIgniter project and you, the CodeIgniter user.
+Instalasi
+1. Clone project
+2. Buat database di phpMyAdmin
+3. Import file `.sql`
+4. Edit konfigurasi database: application/config/database.php
+5. Jalankan di browser: http://localhost/nama_project
